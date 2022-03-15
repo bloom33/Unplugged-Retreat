119a541a310ea14e65cf0efe55e8a01bd4b8ea72
bloom <bloomart11@gmail.com>
2022-03-14 22:33:59 -0400

    Attempted to fix styles; working on getting menu links to work and 'sticky' div to render.

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,10 +7,10 @@ class NavBar extends React.Component {
   
     render() {  
       return (
-       
+          <div className="sticky">
             <div className="header-nav">
                     <figure className="header-figure">
-                    <a href="index.html" target="_blank">
+                    <a href="../index.html" target="_blank"> {/*NOTE: Get link to work */}
                     <img src={require("./images/unpluggedlogo.png")} alt="unplugged logo" />
                     </a>
                     </figure>
@@ -18,11 +18,12 @@ class NavBar extends React.Component {
                     
                     <nav>
                     <ul className="header-list ">
-                    <li className="header-list-item"><a href="faq/index.html" target="_blank">FAQ</a></li>
-                    <li className="header-list-item"><a href="about/index.html" target="_blank">ABOUT</a></li>
+                    <li className="header-list-item"><a href="../faq/index.html" target="_blank">FAQ</a></li> {/*NOTE: Get link to work */}
+                    <li className="header-list-item"><a href="../about/index.html" target="_blank">ABOUT</a></li> {/*NOTE: Get link to work */}
                     </ul>
                     </nav>
                 </div>
+            </div>
        
       
       );
